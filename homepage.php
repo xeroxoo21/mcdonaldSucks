ee690eca14ae76fa4b95657ead399583cb1159c6
datcjordan <imjordantan@gmail.com>
2017-06-12 19:43:40 +0800

    cart design

---
+++ homepage.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Homepage</title>
+	<link href="main.css" rel="stylesheet">
+		<!-- Latest compiled and minified CSS -->
+
+</head>
+<body>
+
+	<?php
+	?>
+
+	<nav>
+			<a href="#">Cart</a>
+			<div class="hover-box">
+				<div class="cart">
+					<h1 class="title">Item(s) in My Cart</h1>
+					<div class="items">
+						<div class="item">
+							<img src="images/fries.png">
+							<div class="item-details">
+								<p>Cheeseburger set x 1</p>
+								<div class="control-n-price">
+									<p>Remove</p>
+									<p>RM9.90</p>
+								</div>
+							</div>
+						</div> <!-- end of item -->
+					
+						<div class="item">
+							<img src="images/hamburguer.png">
+							<div class="item-details">
+								<p>Cheeseburger set x 1</p>
+								<div class="control-n-price">
+									<p>Remove</p>
+									<p>RM9.90</p>
+								</div>
+							</div>
+						</div> <!-- end of item -->
+
+						<div class="item">
+							<img src="images/sandwich.png">
+							<div class="item-details">
+								<p>Cheeseburger set x 1</p>
+								<div class="control-n-price">
+									<p>Remove</p>
+									<p>RM9.90</p>
+								</div>
+							</div>
+						</div> <!-- end of item -->
+
+							<div class="item">
+							<img src="images/sandwich.png">
+							<div class="item-details">
+								<p>Cheeseburger set x 1</p>
+								<div class="control-n-price">
+									<p>Remove</p>
+									<p>RM9.90</p>
+								</div>
+							</div>
+						</div> <!-- end of item -->
+
+							<div class="item">
+							<img src="images/sandwich.png">
+							<div class="item-details">
+								<p>Cheeseburger set x 1</p>
+								<div class="control-n-price">
+									<p>Remove</p>
+									<p>RM9.90</p>
+								</div>
+							</div>
+						</div> <!-- end of item -->
+
+					</div> <!-- end of items -->
+					<div class="price">
+						<h1>Subtotal</h1>
+						<p>RM 9.90</p>
+					</div>
+					
+					<div class="checkout">
+						<p>Check out now</p>
+					</div>
+				</div> <!-- end of cart -->
+			</div> <!-- end of hover box -->
+	</nav>
+
+	<main>
+	
+	<ul>
+		<li><a href="#">Cheeseburger</a></li>
+		<li><a href="#">Fries</a></li>
+		<li><a href="#">Big Mac</a></li>
+		<li><a href="#">Coconut</a></li>
+	</ul>
+
+	</main>
+
+	<footer>
+		
+	</footer>
+
+</body>
+</html>
